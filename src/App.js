2329c1f6881e94d74aaf5fdbc03a772ca485a4e7
Uladzimir Khadakouski <uladzimir.khadakouski@intive.com>
2021-01-31 13:51:02 +0100

    Home, Link, NewReleases, TopCharts components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import Content from "./components/Content/Content";
 import Header from "./components/Header/Header";
 
 function App() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <Content />
+    </>
+  );
 }
 
 export default App;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,27 +1,73 @@
+import styled from "styled-components";
+
 import applicationIcon from "../../assets/play.png";
 import avatar from "../../assets/avatar.png";
 import "./header.scss";
 
 export default function Header() {
   return (
-    <div className="header">
-      <div className="header__bla">
-        <img className="header__bla__icon" src={applicationIcon} alt="icon" />
-        <div>Open Play Store</div>
-      </div>
-      <div className="header__search">
-        <input
-          className="header__search__input"
-          type="text"
-          placeholder="Search"
-        />
-        <button className="header__search__button" type="button">
-          Search
-        </button>
-      </div>
-      <div className="header__avatar">
-        <img className="header__avatar__image" src={avatar} alt="avatar" />
-      </div>
-    </div>
+    <HeaderDiv>
+      <HeaderCredDiv>
+        <HeaderCredIcon src={applicationIcon} alt="icon" />
+        <div>Open Apple Store</div>
+      </HeaderCredDiv>
+      <HeaderSearch>
+        <HeaderSearchInput type="text" placeholder="Search" />
+        <HeaderSearchBtn type="button">Search</HeaderSearchBtn>
+      </HeaderSearch>
+      <HeaderAvatar>
+        <HeaderAvatarImg src={avatar} alt="avatar" />
+      </HeaderAvatar>
+    </HeaderDiv>
   );
 }
+
+const HeaderDiv = styled.div`
+  width: 100%;
+  height: 56px;
+  background-color: #f1f1f1;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const HeaderCredDiv = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const HeaderCredIcon = styled.img`
+  width: 38px;
+  height: 38px;
+`;
+
+const HeaderSearch = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const HeaderSearchInput = styled.input`
+  margin-left: 20px;
+  height: 30px;
+  width: 592px;
+  border-color: lightgray;
+  border-width: 1px;
+  outline-color: lightskyblue;
+  outline-width: 1px;
+  font-size: 16px;
+`;
+
+const HeaderSearchBtn = styled.button`
+  height: 36px;
+`;
+
+const HeaderAvatar = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const HeaderAvatarImg = styled.img`
+  width: 52px;
+  border-radius: 50%;
+`;
